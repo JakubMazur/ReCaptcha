d26009915634d4ed90204b71c5d584f3d91c5c75
Jakub Mazur <JakubMazur@users.noreply.github.com>
2021-07-09 14:56:27 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
 	targets: [
 		.target(
 			name: "ReCaptcha",
-			dependencies: [], path: "ReCapcha/Classes"),
+			dependencies: [], path: "ReCaptcha/Classes"),
 		.testTarget(
 			name: "ReCaptchaTests",
 			dependencies: ["ReCaptcha"]),
